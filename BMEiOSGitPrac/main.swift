90d651a12d5fb2e993d40d1057c5fa93a28d9c75
Jinglin Liu <liujinglinlele@hotmail.com>
2017-08-27 14:54:30 -0700

    Initial Commit

---
+++ BMEiOSGitPrac/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BMEiOSGitPrac
+//
+//  Created by Jinglin Liu on 8/27/17.
+//  Copyright © 2017 Jinglin Liu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
